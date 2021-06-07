d25412162613870a3ff6b2927401057cbb7a7e91
ugawa <ugawa.yoshihiro+github@gmail.com>
2021-06-07 19:51:56 +0900

    This is to check micro:bit Version 2 or not inside the code.

--- main.ts
+++ main.ts
@@ -1,8 +1,7 @@
 let NEW_VERSION = input.runningTime()
-basic.showNumber(NEW_VERSION)
-if (NEW_VERSION > 200) {
-    NEW_VERSION = 0
-} else {
+if (NEW_VERSION < 200) {
     NEW_VERSION = 1
+} else {
+    NEW_VERSION = 0
 }
 basic.showNumber(NEW_VERSION)
